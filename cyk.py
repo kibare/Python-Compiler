ff1d9903d5a3cc1fe0a277b41e2312435a1ff094
Rizky Akbar Asmaran <16520368@std.stei.itb.ac.id>
2021-11-21 02:55:08 +0700

    buat fungsi cyk

--- cyk.py
+++ cyk.py
@@ -20,58 +20,48 @@ else
     I is not member of language
 '''
 
-maxLength = 100
-terminalMax = 20
-p = [[[0 for i in range(terminalMax)] for i in range(maxLength)] for i in range(maxLength)]
-terminal = [None]
-
-#urutan nonterminal
-nt = {}
-nt['S'] = 1
-nt['A'] = 2
-nt['T'] = 3
-nt['B'] = 4
-nt['C'] = 5
-
-terminal.append([["a"]])
-terminal.append([["b"]])
-terminal.append([["A", "B"]])
-terminal.append([["a"], ["A", "C"]])
-terminal.append([["T", "A"], ["B", "A"], ["A", "B"], ["b"]])
-
-nInput = len(input)
-nTerminal = 5
-
-input = "a"*1000
-if(len(input) == 0):
-    print("LOLOS")
-elif(len(input) >= 1000):
-    print("Gagal")
+def CYK(input, CNF):
+    #initialize
+    length = len(input)
+    terminal = len(CNF)
     
-#pake referensi diatas untuk algoritma cyknya
-for s in range(1, nInput+1):
-    for v in range(1, nTerminal+1):
-        for e in terminal[v]:
-            if(e[0] == input[s-1]):
-                p[1][s][v] = True
-                break
+    parse = [[[0 for i in range(terminal + 1)] for i in range(length + 1)] for i in range(length + 1)]
+    terminal1 = [None]
+    nt = {}
+    
+    #Append
+    for i, variable in enumerate(CNF):
+        nt[variable] = i + 1
+        terminal1.append(CNF[variable])
+        
+    #proses parsing
+    #pake algorithm diatas (wikipedia)
+    for s in range(1, length+1):
+        for v in range(1, terminal+1):
+            for e in terminal1[v]:
+                if(e[0] == input[s-1]):
+                    parse[1][s][v] = True
+                    break
 
-for l in range(2, nInput+1):
-    for s in range(1, (nInput-l+2)):
-        for p in range(1, l):
-            for a in range(1, nTerminal+1):
-                for e in terminal[a]:
-                    if(len(e) != 1):
-                        b = nt[e[0]]
-                        c = nt[e[1]]
-                        if(p[p][s][b] and p[l-p][s+p][c]):
-                            p[l][s][a] = True
-                            break
-                        
-if(p[nInput][1][1]):
-    print("ACC")
-else:
-    print("sintaks error")
+    for l in range(2, length+1):
+        for s in range(1, (length-l+2)):
+            for p in range(1, l):
+                for a in range(1, terminal+1):
+                    for e in terminal1[a]:
+                        if(len(e) != 1):
+                            b = nt[e[0]]
+                            c = nt[e[1]]
+                            if(parse[p][s][b] and parse[l-p][s+p][c]):
+                                parse[l][s][a] = True
+                                break
+    
+    #hasil (sementara)
+    if(parse[length][1][1]):
+        print("ACC")
+    else:
+        print("eRrOr")
+    
+
 
 
 
