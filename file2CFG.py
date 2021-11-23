038c49eef0cf88464189e62c937a393df580bc19
kentlius <kent.delatinos@gmail.com>
2021-11-23 17:19:20 +0700

    add txt to cfg

---
+++ file2CFG.py
@@ -0,0 +1,17 @@
+def txtToCFG(filename):
+    CFG = {}
+    with open(filename, 'r') as f:
+        lines = [line.split('->')
+                    for line in f.read().split('\n')
+                    if len(line.split('->')) == 2]
+        for line in lines:
+            var = line[0].replace(" ", "")
+            rawProds = [rawprod.split() for rawprod in line[1].split('|')]
+            prod = []
+            for rawprod in rawProds:
+                prod.append([" " if itm == "__space__" else
+                             "|" if itm == "__or__" else
+                             "\n" if itm == "__new_line__" else
+                             itm for itm in rawprod])
+            CFG.update({var: prod})
+    return CFG
