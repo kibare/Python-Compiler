4d6f0ac62491c6a2da44b289d69e567b125c7f11
kentlius <kent.delatinos@gmail.com>
2021-11-24 20:33:31 +0700

    final cfg & tc

---
+++ input1.py
@@ -0,0 +1,4 @@
+import numpy as np
+
+4product_rule = rule[PRODUCT_KEY]
+4product_rule = 1
---
+++ input3.py
@@ -0,0 +1,6 @@
+a = int(input())
+b = int(input())
+c = int(input())
+a = a + b * c
+test = satu + dua
+print("selamat datang")
